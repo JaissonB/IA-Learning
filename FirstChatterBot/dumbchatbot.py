3ff35945ad5de8787715d77e505b23021873447e
JaissonB <097319@aluno.uricer.edu.br>
2023-03-07 20:41:54 -0300

    Create a dumb chatbot

---
+++ FirstChatterBot/dumbchatbot.py
@@ -0,0 +1,23 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+#Cria o chatbot
+chatbot = ChatBot("DumbBot")
+#Cria um treinador
+trainer = ListTrainer(chatbot)
+
+talk = ['Oi', 'Ola, tudo bem?',
+        'Tudo bem?', 'Tudo otimo',
+        'Voce gosta de programar?', 'Sim, eu programo em Python',
+        'Voce e gay mano?', 'Todos somos, poucos sabem'
+]
+#Executa o treinamento
+trainer.train(talk)
+
+while True: 
+    question = input("User: ")
+    answer = chatbot.get_response(question)
+
+    if float(answer.confidence) > .5:
+        print('DumbBot: ', answer)
+    else:
+        print("DumbBot: I'm sorry! I don't have an answer for this question.")
